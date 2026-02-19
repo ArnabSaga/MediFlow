0d4ba8764573960b8ed3f2e5749f200a6c1dfa65
ArnabSaga <arnabdey15091@gmail.com>
2026-02-19 12:19:16 +0600

    Creating Function To Upload And Delete File From Cloudinary

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,4 +1,6 @@
-import { v2 as cloudinary } from "cloudinary";
+import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
+import status from "http-status";
+import AppError from "../errorHelpers/AppError";
 import { envVars } from "./env";
 
 cloudinary.config({
@@ -6,5 +8,70 @@ cloudinary.config({
   api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
   api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET,
 });
+
+export const uploadFileToCloudinary = async (
+  buffer: Buffer,
+  fileName: string
+): Promise<UploadApiResponse> => {
+  if (!buffer || !fileName) {
+    throw new AppError(status.BAD_REQUEST, "Buffer and file name are required");
+  }
+
+  const extension = fileName.split(".").pop()?.toLocaleLowerCase();
+
+  const fileNameWithoutExtension = fileName
+    .split(".")
+    .slice(0, -1)
+    .join(".")
+    .toLocaleLowerCase()
+    .replace(/\s+/g, "-")
+    // eslint-disable-next-line no-useless-escape
+    .replace(/[^a-z0-9\-]/g, "");
+
+  const uniqueName =
+    Math.random().toString(36).substring(2) + "-" + Date.now() + "-" + fileNameWithoutExtension;
+
+  const folder = extension === "pdf" ? "pdfs" : "images";
+
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+          public_id: `MediFlow/${folder}/${uniqueName}`,
+          folder: `MediFlow/${folder}`,
+        },
+        (error, result) => {
+          if (error) {
+            return reject(
+              new AppError(status.INTERNAL_SERVER_ERROR, "Failed to upload file to cloudinary")
+            );
+          } else {
+            resolve(result as UploadApiResponse);
+          }
+        }
+      )
+      .end(buffer);
+  });
+};
+
+export const deleteFileFromCloudinary = async (fileUrl: string) => {
+  try {
+    const regex = /\/v\d+\/(.+?)(?:\.[a-zA-z0-9]+)+$/;
+
+    const match = fileUrl.match(regex);
+
+    if (match && match[1]) {
+      const publicId = match[1];
+
+      await cloudinary.uploader.destroy(publicId, { resource_type: "image" });
+
+      console.log(`File ${publicId} deleted from cloudinary`);
+    }
+  } catch (error) {
+    console.log("Error deleting file from cloudinary", error);
+    throw new AppError(status.INTERNAL_SERVER_ERROR, "Failed to delete file from cloudinary");
+  }
+};
 
 export const cloudinaryUpload = cloudinary;
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import status from "http-status";
@@ -5,14 +6,28 @@ import * as z from "zod";
 
 import { envVars } from "../config/env";
 
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 import AppError from "../errorHelpers/AppError";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler = async (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (envVars.NODE_ENV === "development") {
     console.log("Error from global error handler", err);
+  }
+
+  if (req.file) {
+    await deleteFileFromCloudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = req.files.map((file) => file.path);
+    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
   }
 
   let errorSources: TErrorSources[] = [];
