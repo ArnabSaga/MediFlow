77d7610c643fdcf5c6399c82a67d313413882565
ArnabSaga <arnabdey15091@gmail.com>
2026-03-01 02:18:23 +0600

    Fixed alignment

--- src/server.ts
+++ src/server.ts
@@ -3,73 +3,70 @@ import app from "./app";
 import { envVars } from "./app/config/env";
 import { seedSuperAdmin } from "./app/utils/seed";
 
-let server : Server;
-const bootstrap = async() => {
-    try {
-        await seedSuperAdmin();
-        server = app.listen(envVars.PORT, () => {
-            console.log(`Server is running on http://localhost:${envVars.PORT}`);
-        });
-    } catch (error) {
-        console.error('Failed to start server:', error);
-    }
-}
+let server: Server;
+const bootstrap = async () => {
+  try {
+    await seedSuperAdmin();
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is running on http://localhost:${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
 
 //! SIGTERM signal handler
 process.on("SIGTERM", () => {
-    console.log("SIGTERM signal received. Shutting down server...");
+  console.log("SIGTERM signal received. Shutting down server...");
 
-    if(server){
-        server.close(() => {
-            console.log("Server closed gracefully.");
-            process.exit(1);
-        });
-    }
+  if (server) {
+    server.close(() => {
+      console.log("Server closed gracefully.");
+      process.exit(1);
+    });
+  }
 
-    process.exit(1);
-
-})
+  process.exit(1);
+});
 
 //! SIGINT signal handler
 process.on("SIGINT", () => {
-    console.log("SIGINT signal received. Shutting down server...");
+  console.log("SIGINT signal received. Shutting down server...");
 
-    if(server){
-        server.close(() => {
-            console.log("Server closed gracefully.");
-            process.exit(1);
-        });
+  if (server) {
+    server.close(() => {
+      console.log("Server closed gracefully.");
+      process.exit(1);
+    });
+  }
 
-    }
-
-    process.exit(1);
+  process.exit(1);
 });
 
 //! uncaught exception handler
-process.on('uncaughtException', (error) => {
-    console.log("Uncaught Exception Detected... Shutting down server", error);
+process.on("uncaughtException", (error) => {
+  console.log("Uncaught Exception Detected... Shutting down server", error);
 
-    if(server){
-        server.close(() => {
-            process.exit(1);
-        })
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1);
-})
+  process.exit(1);
+});
 
 //! unhandled rejection handler
 process.on("unhandledRejection", (error) => {
-    console.log("Unhandled Rejection Detected... Shutting down server", error);
+  console.log("Unhandled Rejection Detected... Shutting down server", error);
 
-    if(server){
-        server.close(() => {
-            process.exit(1);
-        })
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1);
-})
-
+  process.exit(1);
+});
 
 bootstrap();
